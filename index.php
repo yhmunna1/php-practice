235c5cbdb945cdc9f96eda9887c45befbf4f673b
yhmunna1 <yh.munna.barisal@gmail.com>
2025-07-26 13:05:55 +0600

    php code added

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php echo "Hello World"; ?>
-    <h1>Whats up?</h1>
-</body>
-</html>
+<?php
+
+echo "Hello";
+
+$name = "Munna";
+echo "Nam, {$name}";
