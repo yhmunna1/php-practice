cd9f12c032ebfae915115229180a5139437f27fb
yhmunna1 <yh.munna.barisal@gmail.com>
2025-07-29 15:37:16 +0600

    constant declare

--- index.php
+++ index.php
@@ -4,3 +4,12 @@ echo "Hello \n";
 
 $name = "Munna";
 echo "Nam, {$name}";
+echo"\n";
+echo "Git check \n";
+
+$name = "Yeasin";
+echo "$name \n";
+
+// Declare Constant value (Not change able)
+define('PI', 3.1419);
+echo "Value of PI is " .PI;
