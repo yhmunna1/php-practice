f1caeea4d44717aeadfe11db0fb82392eecea324
yhmunna1 <yh.munna.barisal@gmail.com>
2025-07-31 15:49:46 +0600

    Check const

--- index.php
+++ index.php
@@ -5,8 +5,15 @@ echo "Name: {$name}\n";
 $id = 12345;
 echo"Id: {$id}\n";
 
+// Define constant
 define('Job', "Design");
 echo "Job: " .Job;
 echo "\n";
+
 // Check variable type
 var_dump($id);
+echo "\n";
+
+// Check another const (Declare style)
+const nam = "Yeasin";
+echo nam;
